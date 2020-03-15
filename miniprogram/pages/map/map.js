b89f7582f1cdad59c0328c32e16980ccc2144aac
rohero6 <vipluojunjie@126.com>
2020-03-15 23:50:06 +0800

    修改推荐页面得样式，先写好前端，后期载入数据，后端管理。

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,186 +1,73 @@
-// miniprogram/pages/map/map.js
-var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
-var qqmapsdk;
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    latitude: '',
-    longitude: '',
-    markers: [],
-    keywords: '餐饮'
-  
+    currentData: 0,
   },
-  onSearch:function(e){
-    this.setData({
-      keywords: e.detail
-    });
-    this.searchFood()
-  },
-  toaddress: function (e) {
-    let _this = this
-    console.log(e)
-    var id = e.markerId
-    console.log(id)
-    wx.openLocation({
-      latitude: _this.data.markers[id].latitude,
-      longitude: _this.data.markers[id].longitude,
-    })
-    // wx.navigateTo({
-    //   url: '/pages/index/index',
-    //   success: function (res) { },
-    //   fail: function (res) { },
-    //   complete: function (res) { },
-    // })
-  },
-  searchFood:function(){
-    var that = this;
-    var m = [];
-    // 调用接口
-    // var mark= {
-    //       id: 0,
-    //       latitude: '',
-    //       longitude: '',
-    //       callout: {
-    //           content: "",
-    //           padding: 10,
-    //           display: 'ALWAYS',
-    //           textAlign: 'center'}};
-    qqmapsdk.search({
-      location:that.data.latitude + "," + that.data.longitude,
-      keyword: that.data.keywords,
-      page_size: 20,
-      success: function (res) {
-        console.log(res);
-        for (var i = 0; i < res.data.length; i++) {
-          var marker = {}
-          marker.iconPath = '../../images/位置.png'
-          marker.latitude = res.data[i].location.lat;
-          marker.longitude = res.data[i].location.lng;
-          marker.id = i
-          marker.width = 40
-          marker.height = 40
-          marker.label = {
-            content: "",
-            padding: 3,
-            x: 20,
-            y: -40,
-            color: '#ffffff',  //文本颜色
-            borderRadius: 5,  //边框圆角
-            borderWidth: 1,  //边框宽度
-            borderColor: '#CC7033',  //边框颜色
-            bgColor: '#CC7033',  //背景色
-            display: 'ALWAYS',
-            textAlign: 'center'
-          }
-          marker.label.content = res.data[i].title
-          console.log(marker)
-          m.push(marker)
-        }
-        //console.log(m)
-        that.setData({
-          markers: m
-        });
-      },
-      fail: function (res) {
-        console.log(res);
-      },
-      complete: function (res) {
-        console.log(res);
-      }
-    });
-  },
-
-
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onload');
-    var that = this;
-    wx.getLocation({
-      success: function(res) {
-        var lat = res.latitude
-        var log = res.longitude
-        that.setData({
-          latitude: lat,
-          longitude: log
-        });
-      },
-    })
-    wx.chooseLocation({
-      success: function (res) {
-       var lat = res.latitude
-       var log = res.longitude
-        that.setData({
-          latitude: lat,
-          longitude: log
-        });
-        
-      },
-    })
-   
-    qqmapsdk = new QQMapWX({
-      key: 'LPHBZ-RDFCU-4NOV4-2LK6R-DHKY5-R5BNO'
-    });
-    var sy = wx.getSystemInfoSync(),
-      mapWidth = sy.windowWidth * 2,
-      mapHeight = sy.windowHeight * 2;
-    this.setData({
-      mapWidth: mapWidth,
-      mapHeight: mapHeight
-    });
   },
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    console.log('onreadey');
-    this.mapCtx = wx.createMapContext('myMap')
-    this.mapCtx.moveToLocation()
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
   },
-  
+  onReady: function () {
+
+  },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    console.log('onshow');
-    that.searchFood()
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.log('onhide');
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log('onUnload');
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('onPullDownRefresh');
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log('onReachBottom');
+
   },
 
   /**
--- miniprogram/pages/recommend/recommend.js
+++ miniprogram/pages/recommend/recommend.js
@@ -1,23 +1,43 @@
-// pages/recommend/recommend.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    foodList: [
+      {name:'牛肉丸',
+      shopName:'牛肉丸世家',
+      category:'牛肉',
+        imageUrl:'https://mmbiz.qpic.cn/mmbiz_jpg/BZUb7VdvqYxOINEuSMGLP5j8osyEn7OTRdrFAOiazFmUYpxVZl50h0VuAvWe3YpWV3k9km1UDQd2kP5ghtNKFcA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1'
+      },
+      {
+        name:'肠粉',
+        shopName:'时光解馋坊',
+        category:'肠粉',
+        imageUrl:'https://mmbiz.qpic.cn/mmbiz_png/pkm06nQ34NR5aDaWqib3EXQvwyxwNcCtzuwWv49CFDdiafzMTNfal19k5PRXtpuqnbokP8FLbVFSm8bNHPQRgHjQ/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1'
+      }
+                ],
   },
-
+  onChange(event) {
+    this.setData({
+      activeNames: event.detail
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
+  //获取当前滑块的index
+  setTab: function (e) {
+    const edata = e.currentTarget.dataset;
+    this.setData({
+      showtab: edata.tabindex,
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+    })
+  },
+  //点击切换，滑块index赋值
+  
   onReady: function () {
 
   },
